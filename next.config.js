72f9e49e74e8febf35f8058aaf56fc5dee4809d0
rasandilikshana <rasandilikshana@gmail.com>
2023-12-04 14:06:01 +0530

    :lock: security headers

--- next.config.js
+++ next.config.js
@@ -25,6 +25,36 @@ const nextConfig = {
 
     return config;
   },
+
+  // Adding security headers
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
+
+// Define security headers
+const securityHeaders = [
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+];
 
 module.exports = nextConfig;
