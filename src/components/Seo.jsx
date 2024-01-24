0f3d3c53dff2a4e95d9ff1d1f04eb50607bb3b1c
rasandilikshana <rasandilikshana@gmail.com>
2024-01-24 10:44:43 +0530

    added sitemap

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 const SEO = ({ pageTitle }) => {
 
-  const metadescription = `Passionate software engineer in Sri Lanka, skilled in HTML,CSS,React,Laravel, and WordPress.Associate at Zuse Technologies.Let's connect,bring your ideas to life!`;
+  const metadescription = `Passionate software engineer in Sri Lanka, skilled in HTML,CSS,React,Laravel and WordPress.Associate at Zuse Technologies.Let's connect,bring your ideas to life`;
 
   const analyticsScript = `
     window.dataLayer = window.dataLayer || [];
@@ -17,6 +17,7 @@ const SEO = ({ pageTitle }) => {
       <title>
         {pageTitle ? `${pageTitle} - Rasan Dilikshana Personal Portfolio` : "Rasan Dilikshana Personal Portfolio"}
       </title>
+      <meta http-equiv='content-language' content='en-gb'/>
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta name="description" content={metadescription} />
       <meta name="robots" content="index, follow" />
