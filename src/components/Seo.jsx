de2a2ef9667da820e639fd18abf30b85b796a274
rasandilikshana <rasandilikshana@gmail.com>
2024-07-01 19:55:15 +0530

    bug fix

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -5,7 +5,8 @@ import { useRouter } from 'next/router';
 const SEO = ({ pageTitle, metaDescription, metaKeywords, breadcrumb }) => {
   const router = useRouter();
   const baseUrl = "https://rasandilikshana.com";
-  const currentUrl = `${baseUrl}${router.asPath}.html`;
+  const path = router.asPath === "/" ? "/" : `${router.asPath}.html`;
+  const currentUrl = `${baseUrl}${path}`;
 
   const defaultDescription = "Personal portfolio of Rasan Dilikshana, skilled in HTML, CSS, React, Laravel, WordPress, SEO. Associate at Zuse Technologies. Let's connect and bring your ideas to life.";
   const defaultKeywords = "Rasan Dilikshana, software engineer Sri Lanka, web developer Sri Lanka, React developer Sri Lanka, Sri Lanka tech portfolio";
